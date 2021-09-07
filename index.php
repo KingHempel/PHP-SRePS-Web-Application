904a0344077d0480870f87904586566724d24900
102115362 <89816275+102115362@users.noreply.github.com>
2021-09-08 08:37:01 +1000

    Creating the pages we will require for sprint one

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-My new commit
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>PHP-SRePS</title>
+	
+</head>
+
+<body>
+	<h1>PHP-SRePS</h1>
+	
+	<?php
+		
+	?>
+</body>
+</html>
