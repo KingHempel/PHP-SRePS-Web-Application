3f194a17612a265048e03dfc5837b39755c95d23
102115362 <89816275+102115362@users.noreply.github.com>
2021-09-27 13:59:40 +1000

    Adding an About Us page

---
+++ aboutUs.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+  <meta name="description" content="Managing Software Projects" />
+  <meta name="author" content="Charlie Sargeant, Ed Sargeant, Jack Swanton, Kelvin Fu, Riley Hempel" />
+
+  <link href="style.css" rel="stylesheet" type="text/css" />
+
+  <title>PHP-SRePS</title>
+</head>
+
+<body>
+  <!-- HEADER -->
+  <?php
+  $page = 'about';
+  include_once("includes/header.inc");
+  ?>
+
+	<h1>About PHP-SRePS</h1>
+	<div>
+        <p> We are a small independent Chemist available 7 days a week to cater for all your requirements.</p>
+        <br />
+        <p> If you wish to get in contact with us you can use the information below </p>
+        <br />
+        <p> Our email address is info@PHP-SRePS.com </p>
+        <br />
+        <p> Our Mobile Number is 03 9837 1425 </p>
+
+  <!-- FOOTER -->
+  <?php
+  include_once("includes/footer.inc");
+  ?>
+</body>
+</html>
