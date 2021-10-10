f315c47a3f25e84bf8cf713e29026c95c95c690c
102115362 <89816275+102115362@users.noreply.github.com>
2021-10-11 09:01:13 +1100

    Started creating a display monthly report page

---
+++ displayReports.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+  	<meta name="description" content="Managing Software Projects" />
+  	<meta name="author" content="Charlie Sargeant, Ed Sargeant, Jack Swanton, Kelvin Fu, Riley Hempel" />
+
+	<link href="style.css" rel="stylesheet" type="text/css" />
+
+	<title>PHP-SRePS</title>
+</head>
+
+<body>
+	<!-- HEADER -->
+	<?php
+	include_once("includes/header.inc");
+	?>
+	<h1>Sales Report</h1>
+
+    <div>
+
+    <form action = "displayReportsProcess.php" method = "post" >
+			<p>Generate a Monthly Report</p>
+			<input type="submit" value="Submit">
+		</form>
+
+
+		<?php
+		include_once("includes/displaytable.inc");
+		?>
+	</div>
+	
+	<!-- FOOTER -->
+	<?php
+	include_once("includes/footer.inc");
+	?>
+</body>
+</html>
